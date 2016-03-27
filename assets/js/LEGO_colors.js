0b960c43d581d9d3498391a0283442c7b5d7a7a8
Louis Charette <charette.louis@gmail.com>
2016-03-27 14:01:48 -0400

    Updated colours definition files
    Fix for all LEGO Colours ID

--- assets/js/LEGO_colors.js
+++ assets/js/LEGO_colors.js
@@ -1,439 +1,504 @@
+/* References:
+	- http://lego.wikia.com/wiki/Colour_Palette
+	- http://www.brickowl.com/colors
+	- http://danny316p.github.io/LEGO-Colors/output.html
+	- http://swooshable.com/parts/colors
+*/
 var LEGO_Color = {
-  "1": {
-    "LegoID": "White",
-    "Name": "White"
-  },
-  "2": {
-    "LegoID": "Grey",
-    "Name": "Grey"
-  },
-  "3": {
-    "LegoID": "Light yellow",
-    "Name": "Light yellow"
-  },
-  "4": {
-    "LegoID": "Brick Red",
-    "Name": "Brick Red"
-  },
-  "5": {
-    "LegoID": "Brick-yel",
-    "Name": "Brick yellow"
-  },
-  "6": {
-    "LegoID": "Light green",
-    "Name": "Light green"
-  },
-  "11": {
-    "LegoID": "Pastel Blue",
-    "Name": "Pastel Blue"
-  },
-  "12": {
-    "LegoID": "Light Orange Brown",
-    "Name": "Light Orange Brown"
-  },
-  "16": {
-    "LegoID": "Pink",
-    "Name": "Pink"
-  },
-  "18": {
-    "LegoID": "Nougat",
-    "Name": "Nougat"
-  },
-  "20": {
-    "LegoID": "Nature",
-    "Name": "Nature"
-  },
-  "21": {
-    "LegoID": "Br.red",
-    "Name": "Bright red"
-  },
-  "22": {
-    "LegoID": "Med. Reddish Violet",
-    "Name": "Medium Reddish Violet"
-  },
-  "23": {
-    "LegoID": "Br.Blue",
-    "Name": "Bright Blue"
-  },
-  "24": {
-    "LegoID": "Br.yel",
-    "Name": "Bright yellow"
-  },
-  "25": {
-    "LegoID": "Earth-Ora",
-    "Name": "Earth Orange"
-  },
-  "26": {
-    "LegoID": "Black",
-    "Name": "Black"
-  },
-  "27": {
-    "LegoID": "Dk. grey",
-    "Name": "Dark grey"
-  },
-  "28": {
-    "LegoID": "Dk.green",
-    "Name": "Dark green"
-  },
-  "29": {
-    "LegoID": "Med. green",
-    "Name": "Medium green"
-  },
-  "36": {
-    "LegoID": "Light Yellowish Orange",
-    "Name": "Light Yellowish Orange"
-  },
-  "37": {
-    "LegoID": "Br.Green",
-    "Name": "Bright Green"
-  },
-  "38": {
-    "LegoID": "Dk. Orange",
-    "Name": "Dark Orange"
-  },
-  "39": {
-    "LegoID": "Light bluish violet",
-    "Name": "Light bluish violet"
-  },
-  "40": {
-    "LegoID": "Tr.",
-    "Name": "Transparent"
-  },
-  "41": {
-    "LegoID": "Tr.Red",
-    "Name": "Tr. Red"
-  },
-  "42": {
-    "LegoID": "Tr. Lg blue",
-    "Name": "Tr. Lg blue"
-  },
-  "43": {
-    "LegoID": "Tr. Blue",
-    "Name": "Tr. Blue"
-  },
-  "44": {
-    "LegoID": "Tr. Yellow",
-    "Name": "Tr. Yellow"
-  },
-  "45": {
-    "LegoID": "Light blue",
-    "Name": "Light blue"
-  },
-  "47": {
-    "LegoID": "Tr. Flu. Reddish orange",
-    "Name": "Tr. Flu. Reddish orange"
-  },
-  "48": {
-    "LegoID": "Tr. Green",
-    "Name": "Tr. Green"
-  },
-  "49": {
-    "LegoID": "Tr. Flu. Green",
-    "Name": "Tr. Flu. Green"
-  },
-  "50": {
-    "LegoID": "Phosp. White",
-    "Name": "Phosp. White"
-  },
-  "100": {
-    "LegoID": "Light red",
-    "Name": "Light red"
-  },
-  "102": {
-    "LegoID": "Med. blue",
-    "Name": "Medium blue"
-  },
-  "103": {
-    "LegoID": "Light grey",
-    "Name": "Light grey"
-  },
-  "104": {
-    "LegoID": "Br.violet",
-    "Name": "Bright violet"
-  },
-  "105": {
-    "LegoID": "Br. yellowish orange",
-    "Name": "Br. yellowish orange"
-  },
-  "106": {
-    "LegoID": "Br.orange",
-    "Name": "Bright orange"
-  },
-  "107": {
-    "LegoID": "Br.bluish green",
-    "Name": "Bright bluish green"
-  },
-  "110": {
-    "LegoID": "Br.Bluish Violet",
-    "Name": "Bright Bluish Violet"
-  },
-  "111": {
-    "LegoID": "Tr.Brown",
-    "Name": "Tr. Brown"
-  },
-  "112": {
-    "LegoID": "Med. Bluish Violet",
-    "Name": "Medium Bluish Violet"
-  },
-  "113": {
-    "LegoID": "Tr. Medi. reddish violet",
-    "Name": "Tr. Medi. reddish violet"
-  },
-  "114": {
-    "LegoID": "Tr. Med. Reddish-Violet w. Glitter 2%",
-    "Name": "Tr. Medium Reddish-Violet w. Glitter 2%"
-  },
-  "115": {
-    "LegoID": "Med. yellowish green",
-    "Name": "Med. yellowish green"
-  },
-  "116": {
-    "LegoID": "Med. bluish green",
-    "Name": "Med. bluish green"
-  },
-  "117": {
-    "LegoID": "Tr.Glitter",
-    "Name": "Transparent Glitter"
-  },
-  "118": {
-    "LegoID": "Light bluish green",
-    "Name": "Light bluish green"
-  },
-  "119": {
-    "LegoID": "Br. yellowish green",
-    "Name": "Br. yellowish green"
-  },
-  "120": {
-    "LegoID": "Lig. yellowish green",
-    "Name": "Lig. yellowish green"
-  },
-  "124": {
-    "LegoID": "Br.reddish violet",
-    "Name": "Bright reddish violet"
-  },
-  "125": {
-    "LegoID": "Light Orange",
-    "Name": "Light Orange"
-  },
-  "126": {
-    "LegoID": "Tr. Br.bluish violet",
-    "Name": "Tr. Bright bluish violet"
-  },
-  "127": {
-    "LegoID": "Gold",
-    "Name": "Gold"
-  },
-  "128": {
-    "LegoID": "Dk. Nougat",
-    "Name": "Dark Nougat"
-  },
-  "129": {
-    "LegoID": "Tr. Br.Bluish Violet w. Glitter 2%",
-    "Name": "Tr. Bright Bluish Violet w. Glitter 2%"
-  },
-  "131": {
-    "LegoID": "Silver",
-    "Name": "Silver"
-  },
-  "132": {
-    "LegoID": "[Black Glitter]",
-    "Name": "[Black Glitter]"
-  },
-  "135": {
-    "LegoID": "Sand blue",
-    "Name": "Sand blue"
-  },
-  "136": {
-    "LegoID": "Sand violet",
-    "Name": "Sand violet"
-  },
-  "138": {
-    "LegoID": "Sand yellow",
-    "Name": "Sand yellow"
-  },
-  "139": {
-    "LegoID": "Copper",
-    "Name": "Copper"
-  },
-  "140": {
-    "LegoID": "Earth blue",
-    "Name": "Earth blue"
-  },
-  "141": {
-    "LegoID": "Earth Green",
-    "Name": "Earth Green"
-  },
-  "143": {
-    "LegoID": "Tr. Flu. Blue",
-    "Name": "Tr. Flu. Blue"
-  },
-  "145": {
-    "LegoID": "Sand blue metallic",
-    "Name": "Sand blue metallic"
-  },
-  "148": {
-    "LegoID": "Metallic Dk. Grey",
-    "Name": "Metallic Dark Grey"
-  },
-  "151": {
-    "LegoID": "Sand green",
-    "Name": "Sand green"
-  },
-  "153": {
-    "LegoID": "Sand red",
-    "Name": "Sand red"
-  },
-  "154": {
-    "LegoID": "Dk. red",
-    "Name": "Dark red"
-  },
-  "157": {
-    "LegoID": "Tr.Fluorescent Yellow",
-    "Name": "Transparent Fluorescent Yellow"
-  },
-  "158": {
-    "LegoID": "Tr. Flu. Red",
-    "Name": "Tr. Flu. Red"
-  },
-  "176": {
-    "LegoID": "Red Flip/Flop",
-    "Name": "Red Flip/Flop"
-  },
-  "178": {
-    "LegoID": "Yellow Flip/Flop",
-    "Name": "Yellow Flip/Flop"
-  },
-  "179": {
-    "LegoID": "Silver flip/flop",
-    "Name": "Silver flip/flop"
-  },
-  "180": {
-    "LegoID": "Curry",
-    "Name": "Curry"
-  },
-  "182": {
-    "LegoID": "Tr. Br.Orange",
-    "Name": "Tr. Bright Orange"
-  },
-  "183": {
-    "LegoID": "Metallic White",
-    "Name": "Metallic White"
-  },
-  "187": {
-    "LegoID": "Sand Yellow Metallic",
-    "Name": "Sand Yellow Metallic"
-  },
-  "191": {
-    "LegoID": "Flame yellowish orange",
-    "Name": "Flame yellowish orange"
-  },
-  "192": {
-    "LegoID": "Red. Brown",
-    "Name": "Reddish Brown"
-  },
-  "194": {
-    "LegoID": "Med. St-Grey",
-    "Name": "Medium stone grey"
-  },
-  "196": {
-    "LegoID": "Dk. Royal blue",
-    "Name": "Dark Royal blue"
-  },
-  "198": {
-    "LegoID": "Br.Reddish Lilac",
-    "Name": "Bright Reddish Lilac"
-  },
-  "199": {
-    "LegoID": "Dk. St. Grey",
-    "Name": "Dark stone grey"
-  },
-  "200": {
-    "LegoID": "Lemon Metallic",
-    "Name": "Lemon Metallic"
-  },
-  "208": {
-    "LegoID": "Light stone grey",
-    "Name": "Light stone grey"
-  },
-  "212": {
-    "LegoID": "Light Royal blue",
-    "Name": "Light Royal blue"
-  },
-  "216": {
-    "LegoID": "Rust",
-    "Name": "Rust"
-  },
-  "217": {
-    "LegoID": "Brown",
-    "Name": "Brown"
-  },
-  "221": {
-    "LegoID": "Br.Purple",
-    "Name": "Bright Purple"
-  },
-  "222": {
-    "LegoID": "Light Purple",
-    "Name": "Light Purple"
-  },
-  "223": {
-    "LegoID": "Light Pink",
-    "Name": "Light Pink"
-  },
-  "226": {
-    "LegoID": "Cool Yellow",
-    "Name": "Cool Yellow"
-  },
-  "232": {
-    "LegoID": "Dove blue",
-    "Name": "Dove blue"
-  },
-  "268": {
-    "LegoID": "Med. Lilac",
-    "Name": "Medium Lilac"
-  },
-  "283": {
-    "LegoID": "Light Nougat",
-    "Name": "Light Nougat"
-  },
-  "294": {
-    "LegoID": "Phosh. Green",
-    "Name": "Phosh. Green"
-  },
-  "297": {
-    "LegoID": "Warm Gold",
-    "Name": "Warm Gold"
-  },
-  "301": {
-    "LegoID": "[Silver]",
-    "Name": "[Silver]"
-  },
-  "308": {
-    "LegoID": "Dk. Brown",
-    "Name": "Dark Brown"
-  },
-  "309": {
-    "LegoID": "Metalized Gold"
-  },
-  "311": {
-    "LegoID": "Tr.Br.Green",
-    "Name": "Transparent Bright Green"
-  },
-  "312": {
-    "LegoID": "Med. Nougat",
-    "Name": "Medium Nougat"
-  },
-  "315": {
-    "LegoID": "Silver met."
-  },
-  "316": {
-    "LegoID": "Titanium met."
-  },
-  "321": {
-    "LegoID": "Dk. Azur",
-    "Name": "Dark Azur"
-  },
-  "323": {
-    "LegoID": "Aqua",
-    "Name": "Aqua"
+  1: {
+    LegoID: "White",
+    Name: "White"
+  },
+  2: {
+    LegoID: "Grey",
+    Name: "Light Grey"
+  },
+  3: {
+    LegoID: "L.Yel",
+    Name: "Light yellow"
+  },
+  4: {
+    LegoID: "Brick Red",
+    Name: "Brick red"
+  },
+  5: {
+    LegoID: "Brick-yel",
+    Name: "Tan"
+  },
+  6: {
+    LegoID: "L.Green",
+    Name: "Light green"
+  },
+  9: {
+    LegoID: "L.Redviol",
+    Name: "Pink"
+  },
+  11: {
+    LegoID: "PastBlu",
+    Name: "Maersk Blue"
+  },
+  12: {
+    LegoID: "L.Orabrown",
+    Name: "Earth Orange"
+  },
+  16: {
+    LegoID: "Pink",
+    Name: "Pink"
+  },
+  18: {
+    LegoID: "Nougat",
+    Name: "Flesh"
+  },
+  20: {
+    LegoID: "Nature",
+    Name: "Milky White"
+  },
+  21: {
+    LegoID: "Br.red",
+    Name: "Red"
+  },
+  22: {
+    LegoID: "Md.Redviol",
+    Name: "Dark Pink"
+  },
+  23: {
+    LegoID: "Br.Blue",
+    Name: "Blue"
+  },
+  24: {
+    LegoID: "Br.yel",
+    Name: "Yellow"
+  },
+  25: {
+    LegoID: "Earth-Ora",
+    Name: "Brown"
+  },
+  26: {
+    LegoID: "Black",
+    Name: "Black"
+  },
+  27: {
+    LegoID: "Dk.grey",
+    Name: "Dark grey"
+  },
+  28: {
+    LegoID: "Dk.green",
+    Name: "Green"
+  },
+  29: {
+    LegoID: "Md.Green",
+    Name: "Medium Green"
+  },
+  36: {
+    LegoID: "Light Yellowish Orange",
+    Name: "Very Light Orange"
+  },
+  37: {
+    LegoID: "Br.Green",
+    Name: "Bright Green"
+  },
+  38: {
+    LegoID: "DK.ORA",
+    Name: "Dark Orange"
+  },
+  39: {
+    LegoID: "L.Blueviol",
+    Name: "Light Violet"
+  },
+  40: {
+    LegoID: "Tr.",
+    Name: "Trans-Clear"
+  },
+  41: {
+    LegoID: "Tr.Red",
+    Name: "Trans-Red"
+  },
+  42: {
+    LegoID: "Tr.L.Blue",
+    Name: "Trans-Light blue"
+  },
+  43: {
+    LegoID: "Tr.Blue",
+    Name: "Trans-Dark Blue"
+  },
+  44: {
+    LegoID: "Tr.Yel",
+    Name: "Trans-Yellow"
+  },
+  45: {
+    LegoID: "L.Blue",
+    Name: "Light blue"
+  },
+  47: {
+    LegoID: "TR.FL.REDORA",
+    Name: "Trans-Neon Orange"
+  },
+  48: {
+    LegoID: "TR.GREEN",
+    Name: "Trans-Green"
+  },
+  49: {
+    LegoID: "Tr.Fl.Green",
+    Name: "Trans-Neon Green"
+  },
+  50: {
+    LegoID: "Phos.White",
+    Name: "Glow In Dark Opaque"
+  },
+  100: {
+    LegoID: "Light red",
+    Name: "Light Salmon"
+  },
+  102: {
+    LegoID: "Md.Blue",
+    Name: "Medium blue"
+  },
+  103: {
+    LegoID: "L.Grey",
+    Name: "Very Light Gray"
+  },
+  104: {
+    LegoID: "Br. Violet",
+    Name: "Purple"
+  },
+  105: {
+    LegoID: "Br.Yelora",
+    Name: "Medium Orange"
+  },
+  106: {
+    LegoID: "Br.orange",
+    Name: "Orange"
+  },
+  107: {
+    LegoID: "Br.Bluegreen",
+    Name: "Dark Turquoise"
+  },
+  110: {
+    LegoID: "Br.Blueviol",
+    Name: "Violet"
+  },
+  111: {
+    LegoID: "Tr.Brown",
+    Name: "Trans-Black"
+  },
+  112: {
+    LegoID: "Ml.Blueviol",
+    Name: "Medium Violet"
+  },
+  113: {
+    LegoID: "TR.ML.R.VIOL",
+    Name: "Trans-Dark Pink"
+  },
+  114: {
+    LegoID: "Tr. M.R.V.Gli",
+    Name: "Glitter Trans-Dark Pink"
+  },
+  115: {
+    LegoID: "MD.YEL-GREEN",
+    Name: "Medium Lime"
+  },
+  116: {
+    LegoID: "Med.bl-green",
+    Name: "Light Turquoise"
+  },
+  117: {
+    LegoID: "Tr. W.Glitter",
+    Name: "Glitter Trans-Clear"
+  },
+  118: {
+    LegoID: "L.Blue-Green",
+    Name: "Aqua"
+  },
+  119: {
+    LegoID: "Br.Yel-Green",
+    Name: "Lime"
+  },
+  120: {
+    LegoID: "L.Yel-Green",
+    Name: "Light Lime"
+  },
+  124: {
+    LegoID: "Br.Red-Viol",
+    Name: "Magenta"
+  },
+  125: {
+    LegoID: "Light Orange",
+    Name: "Light Orange"
+  },
+  126: {
+    LegoID: "TR.KL.BL.VIO",
+    Name: "Trans-Purple"
+  },
+  127: {
+    LegoID: "Gold",
+    Name: "Pearl Light Gold (Gold)"
+  },
+  128: {
+    LegoID: "DARK NOUGAT",
+    Name: "Dark Nougat (Orange-brown)"
+  },
+  129: {
+    LegoID: "Tr. Br.Bl.V.G",
+    Name: "Glitter Trans-Purple"
+  },
+  131: {
+    LegoID: "Silver",
+    Name: "Pearl Light Gray (Silver)"
+  },
+  132: {
+    LegoID: "[Black Glitter]",
+    Name: "Speckle Black-Silver"
+  },
+  135: {
+    LegoID: "Sand blue",
+    Name: "Sand Blue"
+  },
+  136: {
+    LegoID: "Sand violet",
+    Name: "Sand Purple"
+  },
+  138: {
+    LegoID: "Sand yellow",
+    Name: "Dark tan"
+  },
+  139: {
+    LegoID: "Copper",
+    Name: "Copper"
+  },
+  140: {
+    LegoID: "Earth blue",
+    Name: "Dark Blue"
+  },
+  141: {
+    LegoID: "Earth Green",
+    Name: "Dark Green"
+  },
+  143: {
+    LegoID: "TR.FL. BLUE",
+    Name: "Trans-Medium Blue"
+  },
+  145: {
+    LegoID: "Met.Sand.Blu",
+    Name: "Metal Blue"
+  },
+  147: {
+    LegoID: "Met.Sand.Yel",
+    Name: "Flat Dark Gold"
+  },
+  148: {
+    LegoID: "Met.Dk.Grey",
+    Name: "Pearl Dark Gray (Metallic Dark Grey)"
+  },
+  151: {
+    LegoID: "Sand green",
+    Name: "Sand Green"
+  },
+  153: {
+    LegoID: "Sand red",
+    Name: "Sand Red"
+  },
+  154: {
+    LegoID: "NEW DARK RED",
+    Name: "Dark Red"
+  },
+  157: {
+    LegoID: "Tr.Fluorescent Yellow",
+    Name: "Trans-Neon Yellow"
+  },
+  158: {
+    LegoID: "Tr. Flu. Red",
+    Name: "Trans-Dark Pink"
+  },
+  176: {
+    LegoID: "Red Flip/Flop",
+    Name: "Red Flip/Flop"
+  },
+  178: {
+    LegoID: "Yellow Flip/Flop",
+    Name: "Yellow Flip/Flop"
+  },
+  179: {
+    LegoID: "Silver flip/flop",
+    Name: "Silver flip/flop"
+  },
+  180: {
+    LegoID: "Curry",
+    Name: "Dark Yellow (Curry)"
+  },
+  182: {
+    LegoID: "TR. BR. ORANGE",
+    Name: "Trans-Orange"
+  },
+  183: {
+    LegoID: "Met. White",
+    Name: "Pearl White"
+  },
+  187: {
+    LegoID: "Met. Ear.Ora",
+    Name: "Flat Dark Gold"
+  },
+  191: {
+    LegoID: "Fl. Yell-Ora",
+    Name: "Bright light orange"
+  },
+  192: {
+    LegoID: "Red. Brown",
+    Name: "Reddish Brown"
+  },
+  194: {
+    LegoID: "Med. St-Grey",
+    Name: "Light Bluish Gray"
+  },
+  195: {
+    LegoID: "R.Blue",
+    Name: "Blue-Violet"
+  },
+  196: {
+    LegoID: "Dk. R.Blue",
+    Name: "Dark Blue-Violet"
+  },
+  198: {
+    LegoID: "Br. Red. Lilac",
+    Name: "Light Purple"
+  },
+  199: {
+    LegoID: "Dk. St. Grey",
+    Name: "Dark Bluish Gray"
+  },
+  200: {
+    LegoID: "Lemon Metallic",
+    Name: "Metallic Green"
+  },
+  208: {
+    LegoID: "Lgh. St. Grey",
+    Name: "Very Light Bluish Gray"
+  },
+  212: {
+    LegoID: "Lgh. Roy. Blue",
+    Name: "Bright Light Blue"
+  },
+  216: {
+    LegoID: "Rust",
+    Name: "Rust"
+  },
+  217: {
+    LegoID: "Brown",
+    Name: "Dark Flesh"
+  },
+  219: {
+    LegoID: "Lilac",
+    Name: "Medium Violet"
+  },
+  221: {
+    LegoID: "Br. Purple",
+    Name: "Dark Pink"
+  },
+  222: {
+    LegoID: "LGH. PURPLE",
+    Name: "Bright Pink"
+  },
+  223: {
+    LegoID: "Light Pink",
+    Name: "Light Pink"
+  },
+  226: {
+    LegoID: "Cool Yel.",
+    Name: "Bright Light Yellow"
+  },
+  227: {
+    LegoID: "Tr. Br. Yel gr",
+    Name: "Trans-Bright Green"
+  },
+  229: {
+	LegoID: "Tr. Lgh. Bl. Gr",
+	Name: "Trans-Very Lt Blue"
+  },
+  230: {
+	LegoID: "Tr. Br. Purple",
+	Name: "Trans-Pink",
+  },
+  232: {
+    LegoID: "Do. Blue",
+    Name: "Sky Blue"
+  },
+  236: {
+    LegoID: "Tr. Br. Red. Li",
+    Name: "Trans-Purple"
+  },
+  268: {
+    LegoID: "M. LILAC",
+    Name: "Dark Purple"
+  },
+  283: {
+    LegoID: "L.Nougat",
+    Name: "Light flesh"
+  },
+  294: {
+    LegoID: "PH.GREEN",
+    Name: "Glow In Dark Trans"
+  },
+  296: {
+    LegoID: "C.Silver",
+    Name: "Pearl Light Gray"
+  },
+  297: {
+    LegoID: "W.Gold",
+    Name: "Pearl Gold"
+  },
+  298: {
+    LegoID: "C.SILVER, DR. L",
+    Name: "Metallic Silver"
+  },
+  301: {
+    LegoID: "[Silver]",
+    Name: "[Silver]"
+  },
+  308: {
+    LegoID: "Dk. Brown",
+    Name: "Dark Brown"
+  },
+  309: {
+    LegoID: "Metalized Silver",
+    Name: "Chrome Silver"
+  },
+  310: {
+    LegoID: "Metalized Gold",
+    Name: "Chrome Gold",
+  },
+  311: {
+    LegoID: "TR. BR. GREEN",
+    Name: "Trans-Bright Green"
+  },
+  312: {
+    LegoID: "M. Nougat",
+    Name: "Medium dark flesh"
+  },
+  315: {
+    LegoID: "Silver met.",
+    Name: "Flat Silver"
+  },
+  316: {
+    LegoID: "Titanium met.",
+    Name: "Pearl Dark Gray (Titanium Metallic)"
+  },
+  321: {
+    LegoID: "DARK AZUR",
+    Name: "Dark Azure"
+  },
+  322: {
+    LegoID: "MEDIUM AZUR",
+    Name: "Medium Azure"
+  },
+  323: {
+    LegoID: "Aqua",
+    Name: "Aqua"
+  },
+  326: {
+    LegoID: "SPR. YELL. GREE",
+    Name: "Yellowish Green"
+  },
+  329: {
+    LegoID: "WHITE GLOW",
+    Name: "Glow in Dark White"
   }
 }
