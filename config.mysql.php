cd4d2fde233ef75c2b59dd261ad3a3081a0e03ad
Louis Charette <lcharette@users.noreply.github.com>
2016-04-10 17:30:24 -0400

    Delete config.mysql.php

--- config.mysql.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Configuration MySQL
-$DBhost = "localhost";// Serveur (localhost par default)
-$DBuser = "LEGO";// Nom utilisateur de la DB
-$DBpass = "PVu-Sbg-2R7-epp";// Password DB
-$DBName = "LEGO";// Nom de la DB
-$DBprfx = "LEGO_";// Préfixe des tables
-
-?>
